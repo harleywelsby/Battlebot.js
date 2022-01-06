5a364614fb86da0a3167eb5542b8ff93339f1635
Harley <harleywelsby9@gmail.com>
2022-01-06 19:24:23 +1300

    Redacted config upload

---
+++ config.js
@@ -0,0 +1,23 @@
+export function token() {
+    return token = '';
+}
+
+export function admin() {
+    return '';
+}
+
+export function LEVEL_CAP() {
+    return 30;
+}
+
+export function MISS_CHANCE() {
+    return 20;
+}
+
+export function SAVE_CHANNEL() {
+    return '';
+}
+
+export function AUTOSAVE_TIME() {
+    return (60000)*60;
+}
